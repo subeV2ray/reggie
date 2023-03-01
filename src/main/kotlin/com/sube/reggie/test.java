4d03888014d0f0008668f5c150abf796c88526b3
sube <qiushuwu@gmail.com>
2023-03-01 14:18:06 +0800

    ..

--- src/main/kotlin/com/sube/reggie/test.java
+++ src/main/kotlin/com/sube/reggie/test.java
@@ -3,11 +3,9 @@ package com.sube.reggie;
 /**
  * @author Sube
  * @version 1.0
- * @date 2023/3/1 13:53
- * @description
  */
 public class test {
     public static void main(String[] args) {
-
+        System.out.println(123);
     }
 }
